208a9cf14658839bad54ca39c1d35b796be286b9
mikael_menezes <mixaelmenezes@gmail.com>
2024-11-19 00:53:48 -0300

    navbar implementado

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,20 +1,20 @@
- export default function navbar(){
+ export default function Navbar(){
      return(
          <>
          <nav>
-   <div class="wrapper">
-     <div class="logo"><a href="#">Logo</a></div>
+   <div className="wrapper">
+     <div className="logo"><a href="#">DCE</a></div>
      <input type="radio" name="slider" id="menu-btn"/>
      <input type="radio" name="slider" id="close-btn"/>
-     <ul class="nav-links">
-       <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
-       <li><a href="#">Home</a></li>
-       <li><a href="#">About</a></li>
+     <ul className="nav-links">
+       <label htmlFor="close-btn" className="btn close-btn"><i className="fas fa-times"></i></label>
+       <li><a href="#">Início</a></li>
+       <li><a href="#">Sobre nós</a></li>
        <li>
-         <a href="#" class="desktop-item">Dropdown Menu</a>
+         <a href="#" className="desktop-item">Dropdown Menu</a>
          <input type="checkbox" id="showDrop"/>
-         <label for="showDrop" class="mobile-item">Dropdown Menu</label>
-         <ul class="drop-menu">
+         <label htmlFor="showDrop" className="mobile-item">Dropdown Menu</label>
+         <ul className="drop-menu">
            <li><a href="#">Drop menu 1</a></li>
            <li><a href="#">Drop menu 2</a></li>
            <li><a href="#">Drop menu 3</a></li>
@@ -22,35 +22,35 @@
          </ul>
        </li>
        <li>
-         <a href="#" class="desktop-item">Mega Menu</a>
+         <a href="#" className="desktop-item">Mega Menu</a>
          <input type="checkbox" id="showMega"/>
-         <label for="showMega" class="mobile-item">Mega Menu</label>
-         <div class="mega-box">
-           <div class="content">
-             <div class="row">
+         <label htmlFor="showMega" className="mobile-item">Mega Menu</label>
+         <div className="mega-box">
+           <div className="content">
+             <div className="row">
                <img src="https:fadzrinmadu.github.io/hosted-assets/responsive-mega-menu-and-dropdown-menu-using-only-html-and-css/img.jpg" alt=""/>
              </div>
-             <div class="row">
+             <div className="row">
                <header>Design Services</header>
-               <ul class="mega-links">
+               <ul className="mega-links">
                  <li><a href="#">Graphics</a></li>
                  <li><a href="#">Vectors</a></li>
                  <li><a href="#">Business cards</a></li>
                  <li><a href="#">Custom logo</a></li>
                </ul>
              </div>
-             <div class="row">
+             <div className="row">
                <header>Email Services</header>
-               <ul class="mega-links">
+               <ul className="mega-links">
                  <li><a href="#">Personal Email</a></li>
                  <li><a href="#">Business Email</a></li>
                  <li><a href="#">Mobile Email</a></li>
                  <li><a href="#">Web Marketing</a></li>
                </ul>
              </div>
-             <div class="row">
+             <div className="row">
                <header>Security services</header>
-               <ul class="mega-links">
+               <ul className="mega-links">
                  <li><a href="#">Site Seal</a></li>
                  <li><a href="#">VPS Hosting</a></li>
                  <li><a href="#">Privacy Seal</a></li>
@@ -60,16 +60,11 @@
            </div>
          </div>
        </li>
-       <li><a href="#">Feedback</a></li>
+       <li><a href="#">Calouros</a></li>
      </ul>
-     <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+     <label htmlFor="menu-btn" className="btn menu-btn"><i className="fas fa-bars"></i></label>
    </div>
  </nav>
-
- <div class="body-text">
-   <div class="title">Responsive Dropdown and Mega Menu</div>
-   <div class="sub-title">using only HTML & CSS</div>
- </div>
          </>
      )
  }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-<h1></h1>
+<h1>seja bem vindo ao site do dce</h1>
     </>
   );
 }
